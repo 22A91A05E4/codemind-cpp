68a5fd46952372c6d9cb65faf0cd70a0c27ad6c0
22A91A05E4 <119839946+22A91A05E4@users.noreply.github.com>
2023-05-22 08:29:42 +0530

    #include<iostream> using namespace std; int main() {     int n,r=0,rem=0;     scanf("%d",&n);     while(n!=0)     {         rem=n%10;         r=r*10+rem;         n/=10;     }     printf("%d",r);     return 0; }

---
+++ reverse_of_a_numbers.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,r=0,rem=0;
+    scanf("%d",&n);
+    while(n!=0)
+    {
+        rem=n%10;
+        r=r*10+rem;
+        n/=10;
+    }
+    printf("%d",r);
+    return 0;
+}
