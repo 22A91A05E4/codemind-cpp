d049d28017bac210b511983c36dd7376146faa5e
22A91A05E4 <119839946+22A91A05E4@users.noreply.github.com>
2023-05-22 08:12:45 +0530

    #include<iostream> using namespace std; int main() {     int x,rev=0,r;     scanf("%d",&x);     while(x!=0)     {         r=x%10;         rev=rev*10+r;         x/=10;     }     printf("%d",rev); }

---
+++ Reverse_Integer.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x,rev=0,r;
+    scanf("%d",&x);
+    while(x!=0)
+    {
+        r=x%10;
+        rev=rev*10+r;
+        x/=10;
+    }
+    printf("%d",rev);
+}
