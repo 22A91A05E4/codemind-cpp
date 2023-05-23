4d110c044d0e7bfad6b855364b0f22b91d785776
22A91A05E4 <119839946+22A91A05E4@users.noreply.github.com>
2023-05-23 11:55:36 +0530

    #include<iostream> using namespace std; int main() {     int n;     scanf("%d",&n);     int i,a[n],s=0;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(i%2!=0)         {             s=s+a[i];         }     }     printf("%d",s); }

---
+++ Sum_of_Odd_Positions.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int i,a[n],s=0;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(i%2!=0)
+        {
+            s=s+a[i];
+        }
+    }
+    printf("%d",s);
+}
